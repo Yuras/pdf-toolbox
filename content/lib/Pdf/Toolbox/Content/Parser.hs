fb48a8c0a34b2aee6f188cec94f424ecd41a6db3
Yuras Shumovich <shumovichy@gmail.com>
2013-02-17 04:22:00 +0300

    combile and parse content streams for a page

--- content/lib/Pdf/Toolbox/Content/Parser.hs
+++ content/lib/Pdf/Toolbox/Content/Parser.hs
@@ -7,11 +7,13 @@ module Pdf.Toolbox.Content.Parser
 )
 where
 
+import Data.Int
 import Data.Attoparsec.Char8 (Parser)
 import qualified Data.Attoparsec.Char8 as Parser
-import Control.Monad
+import Data.IORef
 import Control.Applicative
 import System.IO.Streams (InputStream)
+import qualified System.IO.Streams as Streams
 import qualified System.IO.Streams.Attoparsec as Streams
 
 import Pdf.Toolbox.Core
@@ -19,13 +21,41 @@ import Pdf.Toolbox.Core.Parsers.Object
 
 import Pdf.Toolbox.Content.Ops
 
+-- | Parse content streams for a page
+parseContentStream :: MonadIO m => RIS -> [StreamFilter] -> [(Stream Int64, Int)] -> PdfE m (InputStream Expr)
+parseContentStream ris filters streams = do
+  is <- combineStreams ris filters streams
+  liftIO $ Streams.parserToInputStream parseContent is
+
+combineStreams :: MonadIO m => RIS -> [StreamFilter] -> [(Stream Int64, Int)] -> PdfE m IS
+combineStreams _ _ [] = liftIO Streams.nullInput
+combineStreams ris filters (x:xs) = do
+  reader <- mkReader x xs
+  ref <- liftIO $ newIORef reader
+  liftIO $ Streams.makeInputStream (doRead ref)
+  where
+  mkReader (s, len) ss = do
+    Stream _ is <- decodedStreamContent ris filters len s
+    return (is, ss)
+  doRead ref = do
+    (is, ss) <- liftIO $ readIORef ref
+    chunk <- liftIO $ Streams.read is
+    case chunk of
+      Nothing ->
+        case ss of
+          [] -> return Nothing
+          (h:t) -> do
+            reader <- runEitherT $ mkReader h t
+            case reader of
+              Left e -> liftIO $ ioError $ userError $ show e
+              Right r -> do
+                liftIO $ writeIORef ref r
+                doRead ref
+      Just c -> return (Just c)
+
 parseContent :: Parser (Maybe Expr)
 parseContent
   = (Parser.skipSpace >> Parser.endOfInput >> return Nothing)
   <|> do
     Parser.skipSpace
     fmap Just $ fmap Obj parseObject <|> fmap (Op . toOp) (Parser.takeWhile1 isRegularChar)
-
--- | Convert steam of bytes to stream of expressions
-parseContentStream :: MonadIO m => Stream IS -> m (Stream (InputStream Expr))
-parseContentStream (Stream dict is) = Stream dict `liftM` liftIO (Streams.parserToInputStream parseContent is)
