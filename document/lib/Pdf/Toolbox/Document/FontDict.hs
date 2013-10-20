169e7f061fc1a9e44f8be05e62620eba9db420f1
Yuras Shumovich <shumovichy@gmail.com>
2013-10-20 23:19:33 +0300

    Load font info

--- document/lib/Pdf/Toolbox/Document/FontDict.hs
+++ document/lib/Pdf/Toolbox/Document/FontDict.hs
@@ -6,13 +6,21 @@ module Pdf.Toolbox.Document.FontDict
 (
   FontDict,
   FontSubtype(..),
-  fontDictSubtype
+  fontDictSubtype,
+  fontDictLoadInfo
 )
 where
 
+import Data.Monoid
+import Data.Functor
+import Control.Monad
+import qualified System.IO.Streams as Streams
+
 import Pdf.Toolbox.Core
+import Pdf.Toolbox.Content
 
 import Pdf.Toolbox.Document.Internal.Types
+import Pdf.Toolbox.Document.Monad
 
 -- | Font subtypes
 data FontSubtype
@@ -34,3 +42,72 @@ fontDictSubtype (FontDict dict) = do
     "Type3" -> return FontType3
     "TrueType" -> return FontTrueType
     _ -> left $ UnexpectedError $ "Unexpected font subtype: " ++ show str
+
+-- | Load font info for the font
+fontDictLoadInfo :: (MonadPdf m, MonadIO m) => FontDict -> PdfE m FontInfo
+fontDictLoadInfo fd@(FontDict fontDict) = do
+  subtype <- fontDictSubtype fd
+  case subtype of
+    FontType0 -> FontInfoComposite <$> loadFontInfoComposite fontDict
+    _ -> FontInfoSimple <$> loadFontInfoSimple fontDict
+
+loadFontInfoComposite :: (MonadPdf m, MonadIO m) => Dict -> PdfE m FIComposite
+loadFontInfoComposite fontDict = do
+  toUnicode <- loadUnicodeCMap fontDict
+  descFont <- do
+    descFontArr <- lookupDict "DescendantFonts" fontDict >>= deref >>= fromObject
+    case descFontArr of
+      Array [o] -> deref o >>= fromObject
+      _ -> left $ UnexpectedError "Unexpected value of DescendantFonts key in font dictionary"
+  defaultWidth <-
+    case lookupDict' "DW" descFont of
+      Nothing -> return 1000
+      Just o -> deref o >>= fromObject >>= realValue
+  widths <-
+    case lookupDict' "W" descFont of
+      Nothing -> return mempty
+      Just o -> deref o >>= fromObject >>= makeCIDFontWidths
+  return $ FIComposite {
+    fiCompositeUnicodeCMap = toUnicode,
+    fiCompositeWidths = widths,
+    fiCompositeDefaultWidth = defaultWidth
+    }
+
+loadFontInfoSimple :: (MonadPdf m, MonadIO m) => Dict -> PdfE m FISimple
+loadFontInfoSimple fontDict = do
+  toUnicode <- loadUnicodeCMap fontDict
+  encoding <-
+    case lookupDict' "Encoding" fontDict of
+      Just (OName "WinAnsiEncoding") -> return $ Just SimpleFontEncodingWinAnsi
+      Just (OName "MacRomanEncoding") -> return $ Just SimpleFontEncodingMacRoman
+      _ -> return Nothing
+  widths <-
+    case lookupDict' "Widths" fontDict of
+      Nothing -> return Nothing
+      Just v -> do
+        Array array <- deref v >>= fromObject
+        widths <- mapM (fromObject >=> realValue) array
+        firstChar <- lookupDict "FirstChar" fontDict >>= fromObject >>= intValue
+        lastChar <- lookupDict "LastChar" fontDict >>= fromObject >>= intValue
+        return $ Just (firstChar, lastChar, widths)
+  return $ FISimple {
+    fiSimpleUnicodeCMap = toUnicode,
+    fiSimpleEncoding = encoding,
+    fiSimpleWidths = widths
+    }
+
+loadUnicodeCMap :: (MonadPdf m, MonadIO m) => Dict -> PdfE m (Maybe UnicodeCMap)
+loadUnicodeCMap fontDict =
+  case lookupDict' "ToUnicode" fontDict of
+    Nothing -> return Nothing
+    Just o -> do
+      ref <- fromObject o
+      toUnicode <- lookupObject ref
+      case toUnicode of
+        OStream s -> do
+          Stream _ is <- streamContent ref s
+          content <- mconcat <$> liftIO (Streams.toList is)
+          case parseUnicodeCMap content of
+            Left e -> left $ UnexpectedError $ "can't parse cmap: " ++ show e
+            Right cmap -> return $ Just cmap
+        _ -> left $ UnexpectedError "ToUnicode: not a stream"
