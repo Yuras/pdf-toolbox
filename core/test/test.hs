2eb295fa08708f307681d8d2b9a6270a97460f04
Yuras Shumovich <shumovichy@gmail.com>
2015-01-10 16:36:49 +0300

    support 1- and 2-digit escapes sequence in literal string
    Fixes #10

--- core/test/test.hs
+++ core/test/test.hs
@@ -7,6 +7,7 @@ where
 
 import qualified Test.XRef
 import qualified Test.Stream
+import qualified Test.Parser
 
 import Test.Hspec
 
@@ -14,3 +15,4 @@ main :: IO ()
 main = hspec $ do
   Test.XRef.spec
   Test.Stream.spec
+  Test.Parser.spec
