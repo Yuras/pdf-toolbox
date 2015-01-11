b5d851f9fa3fcd03dd259e257998474e3559fb4b
Yuras Shumovich <shumovichy@gmail.com>
2015-01-11 22:12:38 +0300

    add test for simple PDF file

---
+++ document/test/test.hs
@@ -0,0 +1,60 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main
+(
+  main
+)
+where
+
+import Control.Exception (bracket, finally)
+import Control.Monad
+import System.IO
+import qualified System.IO.Streams as Streams
+import System.Directory (getTemporaryDirectory, removeFile)
+
+import Pdf.Toolbox.Core
+import qualified Pdf.Toolbox.Document as Pdf
+
+import Test.Hspec
+
+main :: IO ()
+main = hspec $ do
+  describe "simple.pdf" $ do
+    it "should have title" $
+      withSimpleFile $ \h -> do
+      pdf <- Pdf.withHandle h
+      maybe_info <- Pdf.document pdf >>= Pdf.info
+      title <-
+        case maybe_info of
+          Nothing -> return Nothing
+          Just info -> Pdf.title info
+      title `shouldBe` Just "simple PDF file"
+
+-- | Generate simple PDF file for tests
+withSimpleFile :: (Handle -> IO ()) -> IO ()
+withSimpleFile action = do
+  dir <- getTemporaryDirectory
+  bracket
+    (openBinaryTempFile dir "simple.pdf")
+    (\(path, h) -> hClose h `finally` removeFile path)
+    $ \(_, h) -> do
+  out <- Streams.handleToOutputStream h
+  runPdfWriter out $ do
+    writePdfHeader
+    deleteObject (Pdf.Ref 0 1) 65535
+    forM_ objects $ \(ref, obj) ->
+      writeObject ref obj
+    writeXRefTable 0 tr
+  action h
+  where
+  tr = Dict
+    [ ("Size", ONumber $ NumInt $ length objects + 1)
+    , ("Info", ORef infoRef)
+    ]
+  info = Dict
+    [ ("Title", OStr "simple PDF file")
+    ]
+  objects =
+    [ (infoRef, ODict info)
+    ]
+  infoRef = Ref 1 0
