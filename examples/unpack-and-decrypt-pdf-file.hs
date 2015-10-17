12d2ea0f8c6fbb0c085b82e2d4ecf291a65b9016
Yuras Shumovich <shumovichy@gmail.com>
2015-10-17 13:06:18 +0300

    Unbreak unpack-and-decrypt example

--- examples/unpack-and-decrypt-pdf-file.hs
+++ examples/unpack-and-decrypt-pdf-file.hs
@@ -7,49 +7,58 @@ module Main
 where
 
 import Data.Int
-import qualified Data.ByteString.Lazy as BSL
+import qualified Data.ByteString.Lazy as Lazy (ByteString)
+import qualified Data.ByteString.Lazy as Lazy.ByteString
+import qualified Data.Foldable as Foldable
+import qualified Data.Vector as Vector
 import Data.IntSet (IntSet)
 import qualified Data.IntSet as IntSet
+import qualified Data.HashMap.Strict as HashMap
 import Control.Monad
 import Control.Monad.Trans.Class
 import Control.Monad.Trans.State
+import Control.Exception
 import System.Environment
 import System.IO
 import qualified System.IO.Streams as Streams
 
-import Pdf.Toolbox.Core
+import Pdf.Toolbox.Core hiding (rawStreamContent)
 import Pdf.Toolbox.Document
+import Pdf.Toolbox.Document.Encryption
 import Pdf.Toolbox.Document.Internal.Types
 
 main :: IO ()
 main = do
   [input] <- getArgs
-  res <- withBinaryFile input ReadMode $ \handle ->
-    runPdfWithHandle handle knownFilters $
-    -- we take each object only once, and object cache
-    -- will only slow down everything
-    withoutObjectCache $ do
-    encrypted <- isEncrypted
+  withBinaryFile input ReadMode $ \h -> do
+    pdf <- pdfWithHandle h
+
+    encrypted <- isEncrypted pdf
     when encrypted $ do
-      ok <- setUserPassword defaultUserPassword
-      unless ok $ error "Need user password"
-    Document _ tr <- document
+      ok <- setUserPassword pdf defaultUserPassword
+      unless ok $
+        error "Wrong password"
+
+    Document _ tr <- document pdf
 
     let
-        loop :: Object Int64 -> StateT IntSet (PdfWriter (Pdf IO)) ()
-        loop (ODict (Dict vals)) = forM_ vals $ loop . mapObject (error "impossible") . snd
-        loop (OArray (Array vals)) = forM_ vals $ loop . mapObject (error "impossible")
-        loop (ORef r@(Ref index _)) = do
+        loop :: Object Int64 -> StateT IntSet (PdfWriter IO) ()
+        loop (Dict vals) = Foldable.forM_ vals $ \v ->
+          loop (mapObject (error "impossible") v)
+        loop (Array vals) = Vector.forM_ vals $ \v ->
+          loop (mapObject (error "impossible") v)
+        loop (Ref r@(R index _)) = do
           -- check that the object is not already written.
           -- necessary to prevent circles
           member <- gets $ IntSet.member index
           if member
             then return ()
             else do
-              o <- lift $ lift $ lookupObject r
-              lift ( lift $ loadStream r o) >>= lift . writeObject r
+              o <- lift $ lift $ lookupObject pdf r
+              lift (lift (loadStream pdf r o) >>= writeObject r)
               modify $ IntSet.insert index
               loop o
+        loop (Stream (S d _)) = loop (Dict d)
         loop _ = return ()
 
     runPdfWriter Streams.stdout $ do
@@ -57,32 +66,43 @@ main = do
         lift writePdfHeader
         -- traverse all the objects starting from trailer
         -- and write out all the indirect objects found
-        loop (ODict tr)
+        loop (Dict tr)
         -- There are no more xrefs, so clean prev key
-        lift $ writeXRefTable 0 (deleteValueForKey "Prev" tr)
-  case res of
-    Right _ -> return ()
-    Left e -> hPutStrLn stderr (show e)
+        lift $ writeXRefTable 0 (HashMap.delete "Prev" tr)
 
--- Load stream content as lazy bytestring. Try to decode it,
--- but if it is not possible (e.g. streams filter is not found),
--- then load raw content. It may not work with encrypted documents
-loadStream :: Ref -> Object Int64 -> Pdf IO (Object BSL.ByteString)
-loadStream r (OStream s) = loadDecodedStream r s `catchT` \_ -> loadRawStream s
-loadStream _ o = return $ mapObject (error "impossible") o
+    return ()
 
-loadDecodedStream :: Ref -> Stream Int64 -> Pdf IO (Object BSL.ByteString)
-loadDecodedStream r s = do
-  Stream d is <- streamContent r s
-  content <- liftIO $ BSL.fromChunks `liftM` Streams.toList is
+mapObject :: (a -> b) -> Object a -> Object b
+mapObject _ (Dict d) = Dict d
+mapObject _ (Name n) = Name n
+mapObject _ (String s) = String s
+mapObject _ (Number n) = Number n
+mapObject _ (Bool b) = Bool b
+mapObject _ (Array a) = Array a
+mapObject _ (Ref r) = Ref r
+mapObject _ Null = Null
+mapObject f (Stream (S d a)) = Stream (S d (f a))
+
+loadStream :: Pdf -> Ref -> Object Int64 -> IO (Object Lazy.ByteString)
+loadStream pdf ref (Stream s) = do
+  s' <- loadDecodedStream pdf ref s
+    `catch` \Corrupted{} -> loadRawStream pdf ref s
+  return (Stream s')
+loadStream _ _ o = return (mapObject (error "impossible") o)
+
+loadDecodedStream :: Pdf -> Ref -> Stream Int64 -> IO (Stream Lazy.ByteString)
+loadDecodedStream pdf ref s = do
+  S d is <- streamContent pdf ref s
+  cont <- Lazy.ByteString.fromChunks <$> Streams.toList is
   -- update length and remove filter
-  let d' = setValueForKey "Length" (ONumber $ NumInt $ fromIntegral $ BSL.length content) $ deleteValueForKey "Filter" d
-  return $ OStream $ Stream d' content
+  let d' = HashMap.insert "Length" (Number len)
+         . HashMap.delete "Filter"
+         $ d
+      len = fromIntegral (Lazy.ByteString.length cont)
+  return (S d' cont)
 
-loadRawStream :: Stream Int64 -> Pdf IO (Object BSL.ByteString)
-loadRawStream s@(Stream d _) = do
-  l <- lookupDict "Length" d >>= deref >>= fromObject >>= intValue
-  ris <- getRIS
-  Stream _ is <- rawStreamContent ris l s
-  content <- liftIO $ BSL.fromChunks `liftM` Streams.toList is
-  return $ OStream $ Stream d content
+loadRawStream :: Pdf -> Ref -> Stream Int64 -> IO (Stream Lazy.ByteString)
+loadRawStream pdf _ s = do
+  S d is <- rawStreamContent pdf s
+  cont <- Lazy.ByteString.fromChunks <$> Streams.toList is
+  return (S d cont)
