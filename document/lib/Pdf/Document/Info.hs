769132446cd6cbfae8a98804dd0feb4460227844
Yuras Shumovich <shumovichy@gmail.com>
2018-03-06 14:52:04 +0300

    document author, subject, etc

--- document/lib/Pdf/Document/Info.hs
+++ document/lib/Pdf/Document/Info.hs
@@ -4,7 +4,12 @@
 
 module Pdf.Document.Info
 (
-  infoTitle
+  infoTitle,
+  infoAuthor,
+  infoSubject,
+  infoKeywords,
+  infoCreator,
+  infoProducer,
 )
 where
 
@@ -25,3 +30,48 @@ infoTitle (Info pdf _ dict) =
     Just o -> do
       o' <- deref pdf o
       sure $ fmap Just (stringValue o') `notice` "Title should be a string"
+
+-- | The name of the person who created the document
+infoAuthor :: Info -> IO (Maybe ByteString)
+infoAuthor (Info pdf _ dict) =
+  case HashMap.lookup "Author" dict of
+    Nothing -> return Nothing
+    Just o -> do
+      o' <- deref pdf o
+      sure $ fmap Just (stringValue o') `notice` "Author should be a string"
+
+-- | The subject of the document
+infoSubject :: Info -> IO (Maybe ByteString)
+infoSubject (Info pdf _ dict) =
+  case HashMap.lookup "Subject" dict of
+    Nothing -> return Nothing
+    Just o -> do
+      o' <- deref pdf o
+      sure $ fmap Just (stringValue o') `notice` "Subject should be a string"
+
+-- | Keywords associated with the document
+infoKeywords :: Info -> IO (Maybe ByteString)
+infoKeywords (Info pdf _ dict) =
+  case HashMap.lookup "Keywords" dict of
+    Nothing -> return Nothing
+    Just o -> do
+      o' <- deref pdf o
+      sure $ fmap Just (stringValue o') `notice` "Keywords should be a string"
+
+-- | The name of the application that created the original document
+infoCreator :: Info -> IO (Maybe ByteString)
+infoCreator (Info pdf _ dict) =
+  case HashMap.lookup "Creator" dict of
+    Nothing -> return Nothing
+    Just o -> do
+      o' <- deref pdf o
+      sure $ fmap Just (stringValue o') `notice` "Creator should be a string"
+
+-- | The name of the application that converted the document to PDF format
+infoProducer :: Info -> IO (Maybe ByteString)
+infoProducer (Info pdf _ dict) =
+  case HashMap.lookup "Producer" dict of
+    Nothing -> return Nothing
+    Just o -> do
+      o' <- deref pdf o
+      sure $ fmap Just (stringValue o') `notice` "Producer should be a string"
