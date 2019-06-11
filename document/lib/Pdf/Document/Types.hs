d22955ac7adaa7354f8566f91a8bb58d44fdaf8b
Yuras Shumovich <shumovichy@gmail.com>
2019-06-11 18:44:44 +0300

    Fix ghc-8.6 support

--- document/lib/Pdf/Document/Types.hs
+++ document/lib/Pdf/Document/Types.hs
@@ -19,8 +19,9 @@ data Rectangle a = Rectangle a a a a
 
 -- | Create rectangle form an array of 4 numbers
 rectangleFromArray :: Array -> Either String (Rectangle Double)
-rectangleFromArray arr | Vector.length arr == 4 = do
-  [a, b, c, d] <- mapM realValue (Vector.toList arr)
+rectangleFromArray arr = do
+  res <- mapM realValue (Vector.toList arr)
       `notice` "Rectangle should contain real values"
-  return $ Rectangle a b c d
-rectangleFromArray array = Left ("rectangleFromArray: " ++ show array)
+  case res of
+    [a, b, c, d] -> return $ Rectangle a b c d
+    _ -> Left ("rectangleFromArray: " ++ show arr)
