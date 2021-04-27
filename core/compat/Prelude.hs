192fed0ff43d2fcb95f55259b926db9798ef52dd
Yuras <shumovichy@gmail.com>
2021-04-28 00:33:10 +0300

    Cleanup dependencies (#68)

--- core/compat/Prelude.hs
+++ core/compat/Prelude.hs
@@ -5,13 +5,6 @@ module Prelude
 (
   module P,
 
-#if MIN_VERSION_base(4,8,0)
-#else
-  (<$>),
-  Monoid(..),
-  Applicative(..),
-#endif
-
 #if MIN_VERSION_base(4,11,0)
 #else
   Semigroup(..),
@@ -19,18 +12,7 @@ module Prelude
 )
 where
 
-#if MIN_VERSION_base(4,6,0)
 import "base" Prelude as P
-#else
-import "base" Prelude as P hiding (catch)
-#endif
-
-#if MIN_VERSION_base(4,8,0)
-#else
-import Data.Functor((<$>))
-import Data.Monoid(Monoid(..))
-import Control.Applicative(Applicative(..))
-#endif
 
 #if MIN_VERSION_base(4,11,0)
 #else
--- examples/compat/Prelude.hs
+++ examples/compat/Prelude.hs
@@ -5,24 +5,16 @@ module Prelude
 (
   module P,
 
-#if MIN_VERSION_base(4,8,0)
+#if MIN_VERSION_base(4,11,0)
 #else
-  (<$>),
-  Monoid(..),
-  Applicative(..),
+  Semigroup(..),
 #endif
 )
 where
 
-#if MIN_VERSION_base(4,6,0)
 import "base" Prelude as P
-#else
-import "base" Prelude as P hiding (catch)
-#endif
 
-#if MIN_VERSION_base(4,8,0)
+#if MIN_VERSION_base(4,11,0)
 #else
-import Data.Functor((<$>))
-import Data.Monoid(Monoid(..))
-import Control.Applicative(Applicative(..))
+import Data.Semigroup(Semigroup(..))
 #endif
