485d1b32fbab38f9c28ea187604759fac0ca1db5
Yuras Shumovich <shumovichy@gmail.com>
2021-04-28 00:20:04 +0300

    prelude!

--- content/compat/Prelude.hs
+++ content/compat/Prelude.hs
@@ -5,32 +5,12 @@ module Prelude
 (
   module P,
 
-#if MIN_VERSION_base(4,8,0)
-#else
-  (<$>),
-  Monoid(..),
-  Applicative(..),
-#endif
-
 #if MIN_VERSION_base(4,11,0)
 #else
   Semigroup(..),
 #endif
 )
 where
-
-#if MIN_VERSION_base(4,6,0)
-import "base" Prelude as P
-#else
-import "base" Prelude as P hiding (catch)
-#endif
-
-#if MIN_VERSION_base(4,8,0)
-#else
-import Data.Functor((<$>))
-import Data.Monoid(Monoid(..))
-import Control.Applicative(Applicative(..))
-#endif
 
 #if MIN_VERSION_base(4,11,0)
 #else
--- core/compat/Prelude.hs
+++ core/compat/Prelude.hs
@@ -5,15 +5,6 @@ module Prelude
 (
   module P,
 
-{-
-#if MIN_VERSION_base(4,8,0)
-#else
-  (<$>),
-  Monoid(..),
-  Applicative(..),
-#endif
--}
-
 #if MIN_VERSION_base(4,11,0)
 #else
   Semigroup(..),
@@ -22,20 +13,6 @@ module Prelude
 where
 
 import "base" Prelude as P
-{-
-#if MIN_VERSION_base(4,6,0)
-import "base" Prelude as P
-#else
-import "base" Prelude as P hiding (catch)
-#endif
-
-#if MIN_VERSION_base(4,8,0)
-#else
-import Data.Functor((<$>))
-import Data.Monoid(Monoid(..))
-import Control.Applicative(Applicative(..))
-#endif
--}
 
 #if MIN_VERSION_base(4,11,0)
 #else
